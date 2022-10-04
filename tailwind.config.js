dc13b76320fe73c1de98cdb031c9d009a7d84e36
JOMADELEMA <jomadelema@gmail.com>
2022-10-03 21:47:52 -0600

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       backgroundImage: {
         'beach-noon': 'url(/assets/beach-noon.jpg)',
         'cloudy-sky': 'url(/assets/cloudy-sky.jpg)',
+      },
+      colors: {
+        'brighter': '#f0f6f6',
+        'bright': '#dbf0f3',
+        'normal': '#99dbea',
+        'dark': '#5fb0cd',
+        'darken': '#2585b0',
+        'darkest': '#084b83'
       }
     },
   },
